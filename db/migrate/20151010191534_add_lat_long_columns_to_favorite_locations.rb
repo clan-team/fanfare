94678fac6934e3a7be7196cb157236889627b3ac
Cameron Dutro <camertron@gmail.com>
2015-10-10 14:31:04 -0500

    Update lat/long columns to floats in migrations

--- db/migrate/20151010191534_add_lat_long_columns_to_favorite_locations.rb
+++ db/migrate/20151010191534_add_lat_long_columns_to_favorite_locations.rb
@@ -1,6 +1,6 @@
 class AddLatLongColumnsToFavoriteLocations < ActiveRecord::Migration
   def change
-  	add_column :favorite_locations, :location_lat, :integer
-  	add_column :favorite_locations, :location_long, :integer
+  	add_column :favorite_locations, :location_lat, :float, precision: 10, scale: 6
+  	add_column :favorite_locations, :location_long, :float, precision: 10, scale: 6
   end
 end
