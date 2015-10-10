0fbcfd18f68045c74f0241305c62541779053bb7
Nick <ngflanders@gmail.com>
2015-10-10 18:02:34 -0500

    Added login screen and sessions//Not working

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+	def new
+	end
+	def create
+		if u = User.authenticate(user_params)
+			session[:user_id] = u.id
+			redirect_to(root_path, notice: "Logged in!")
+		else
+			redirect_to(new_session_path, alert: "Invalid credentials")
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to(root_path, notice: "Logged out")
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
+  validates :username, uniqueness: true, presence: true
+  validates :password_hash, presence: true
+
   has_and_belongs_to_many :entities
   has_many :favorite_locations
+
+  def password=(new_password)
+  	write_attribute(
+  		:password_hash, Digest::MD5.hexdigest(new_password)
+  	)
+  end
+
+  def self.authenticate(options = {})
+  	params = options.merge(
+  		'password_hash' =>  hash_password(options['password'])
+  	)
+  	params.delete('password')
+  	User.where(params).first
+  end
 end
