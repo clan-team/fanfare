4c307024d642197c2d1db4503b8e194fb20a82be
Annika Erickson <Annika@AnnikaEricksonMBP.local>
2015-10-10 23:30:28 -0500

    resized screen for mobile, added splash-screen file

---
+++ app/assets/javascripts/viewport.js
@@ -0,0 +1,6 @@
+// document.ready(function(){
+// 	if($(window).width() >= 768) {
+//     // Change viewport for smaller devices
+//     $('meta[name=viewport]').attr('content','width=959');
+// 	}
+// });
