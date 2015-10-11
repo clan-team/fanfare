11ea4a9cb6e4f5887734cf437fd315f2dde7d444
Cameron Dutro <camertron@gmail.com>
2015-10-10 23:27:47 -0500

    Entity following working

--- app/assets/javascripts/entity.js
+++ app/assets/javascripts/entity.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-	$('.follow-btn', '.entity').click(function() {
-		$.post
-	});
+  $('.follow-btn', '.entity').click(function() {
+    var button = $(this)
+    var parent = button.closest('.entity');
+    var entityId = $(parent).data('entity-id');
+    var userId = $('body').data('user-id');
+    var route = Routes.entities_users_path({format: 'json'});
+    var data = {user_id: userId, entity_id: entityId};
+
+    button.prop('disabled', true);
+
+    $.post(route, data)
+      .fail(function(err) {
+        button.prop('disabled', false);
+      });
+  });
 });
--- app/controllers/entities_users_controller.rb
+++ app/controllers/entities_users_controller.rb
@@ -4,7 +4,14 @@ class EntitiesUsersController < ApplicationController
 	def create
 		user = User.find(entities_users_params[:user_id])
 		entity = Entity.find(entities_users_params[:entity_id])
-		respond_with user.entities << entity
+
+		unless user.entities.include?(entity)
+			user.entities << entity
+		end
+
+		respond_to do |format|
+			format.json { render json: {} }
+		end
 	end
 
 	private
